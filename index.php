cc5b8fec7244090c90091efb2f08c25af2cae430
Casper <Casper@145-24-200-22.wlan.hro.nl>
2015-03-27 14:16:07 +0100

    test

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <title></title>
+        
+        <meta name="description" content="width=device-width, user-scalable=no" />
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="" />
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        
+    <head>
+    
+    <body>
+    
+    </body>
+</html>
