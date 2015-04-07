e6856d6ba911645caed8f5770487d8683d8e01c7
Sven de Ronde <ewderonde@gmail.com>
2015-04-07 15:54:40 +0100

    nieuwe pagina's aangemaakt

---
+++ group_event_create.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sven
+ * Date: 7-4-2015
+ * Time: 15:49
+ */
---
+++ js/main-solo_event_create.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Sven on 7-4-2015.
+ */
+$(init);
+
+function init(){
+
+}
