3d21a88adb3f2f9834dbbe9a7b7843e816b07c5e
Sven de Ronde <ewderonde@gmail.com>
2015-04-09 15:53:22 +0100

    no message

--- php/playlist.php
+++ php/playlist.php
@@ -3,20 +3,28 @@ $data = json_decode(file_get_contents("https://api.spotify.com/v1/albums/669MIsr
 
 $i = 0;
 echo "<pre>";
-// print_r($data);
+ print_r($data);
 // navigatie naar een track name:
 // $data->items[0]->name
 
-    count($data->items);
+
 //foreach ($data as $file) {
 //    $i++;
 //    print_r($i);
 //    print_r($file[$i]->name);
 //}
-for ($i=0; $i < count($data->items); $i++) {
-    echo "hoi";
-}
+//for ($i=0; $i < count($data->items); $i++) {
+//    $trackData[] = array(
+//        'track'=> $data->items[$i]->name,
+//        'url'=> $data->items[$i]->;
+//    );
+//}
 echo "</pre>";
 
 
 ?>
+<!--$photoValues[] = array(-->
+<!--'image'=> $data->data[$i]->images->thumbnail->url,-->
+<!--'latitude'=> $data->data[$i]->location->latitude,-->
+<!--'longitude'=> $data->data[$i]->location->longitude-->
+<!--);-->
