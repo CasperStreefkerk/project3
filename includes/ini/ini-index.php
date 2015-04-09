b893301977514ae28b8802c4c0caaf5c40b5fa13
Dianchelo <diianchelo@gmail.com>
2015-04-09 14:01:17 +0200

    Solo/group event create

--- includes/ini/ini-index.php
+++ includes/ini/ini-index.php
@@ -17,8 +17,24 @@ if(isset($_POST['submitLogin'])){
         //header("Location: index.php");
     }else{
 
-        $session->message("error", "<span class='red'>JE BENT SUCCESVOL INGELOGD</span>");
+        if($user['type'] == 2){
+            $session->message("error", "<span class='red'>TWEE</span>");
+            header('Location: event_options.php');
+            die;
+        }
+
+        if($user['type'] == 1){
+            $session->message("error", "<span class='red'>TWEE</span>");
+        }
+
+        if($user['type'] == 3){
+            $session->message("error", "<span class='red'>TWEE</span>");
+        }
+
+
+        //$session->message("error", "<span class='red'>JE BENT SUCCESVOL INGELOGD</span>");
         $session->set("id", $user['id']);
+
         header('Location: '.$_SERVER['PHP_SELF']);
         die;
         //header("Location: index.php");
--- solo_event_create.php
+++ solo_event_create.php
@@ -1,7 +1,81 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Sven
- * Date: 7-4-2015
- * Time: 15:49
- */
+
+include_once('header.php');
+
+?>
+
+
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+    <div class="container">
+        <h1>Welkom bij ons project!</h1>
+        <p>Ons project is super mooi. Koop ons product nu voor maar 5 euro.</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Registreer nu! &raquo;</a></p>
+    </div>
+</div>
+
+<div class="container">
+    <!-- Example row of columns -->
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Waarom registreren?</h2>
+            <p>Door te registreren kunt u gebruik gaan maken van onze applicatie. Hierdoor krijgt u een leuke bezigheid, ziet u nieuwe plaatsen in de stad en unlocked u nieuwe exclusieve muziek. Daarnaast is het ook mogelijk om nieuwe mensen te ontmoeten met dezelfde passie voor muziek. </p>
+            <p>Registreren kan hiernaast.</p>
+        </div>
+        <div class="col-md-6">
+            <h2>Registreer hieronder</h2>
+            <form  action="" method="POST">
+                <div class="form-group">
+                    <select class="form-control" id="usertype" name="usertype">
+                        <option value="1">Ik ben een muziekliefhebber</option>
+                        <option value="2">Ik ben een artiest</option>
+                        <option value="3">Ik ben een platenmaatschappij</option>
+                    </select>
+                </div>
+
+
+                    <div class="form-group">
+                        <!--                    <label for="email"></label>-->
+                        <input type="text" class="form-control" id="firstname"  name="firstname" placeholder="Voornaam">
+                    </div>
+                    <div class="form-group">
+                        <!--                    <label for="pwd">Password:</label>-->
+                        <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Achternaam">
+                    </div>
+                    <div class="form-group">
+                        <!--                    <label for="pwd">Password:</label>-->
+                        <input type="text" class="form-control" name="address" id="address" placeholder="Adres">
+                    </div>
+                    <div class="form-group">
+                        <!--                    <label for="pwd">Password:</label>-->
+                        <input type="text" class="form-control" id="place" name="place" placeholder="Woonplaats">
+                    </div>
+                    <div class="form-group">
+                        <!--                    <label for="pwd">Password:</label>-->
+                        <input type="text" class="form-control" name="zipcode" id="zipcode" placeholder="Postcode">
+                    </div>
+                    <div class="form-group">
+                        <!--                    <label for="pwd">Password:</label>-->
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Emailadres">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" placeholder="Wachtwoord" id="password" name="password" class="form-control">
+                    </div>
+
+
+                <button type="submit" name="registerUser" id="registerUser" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+        <!--        <div class="col-md-5">-->
+        <!--            <h2>Functionaliteit 3</h2>-->
+        <!--            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>-->
+        <!--            <p><a class="btn btn-default" href="#" role="button">Lees meer &raquo;</a></p>-->
+        <!--        </div>-->
+    </div>
+
+    <?php
+
+    include_once('footer.php');
+
+    ?>
+
