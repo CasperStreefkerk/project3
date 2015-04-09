23712c61986bb58f31692260fc7b68806252e3f6
Sven de Ronde <ewderonde@gmail.com>
2015-04-10 00:27:45 +0100

    no message

--- js/main-solo_event_create.js
+++ js/main-solo_event_create.js
@@ -4,5 +4,17 @@
 $(init);
 
 function init(){
+    mapping();
+}
 
+function mapping(){
+        console.log('hoi');
+        // De globale variables binnen de mapDrawing functie.
+        // Deze zorgen voor de standaard positie en zoom van de map.
+        var myLatlng = new google.maps.LatLng(51.91804109675677, 4.470727443695068);
+        var mapOptions = {
+            zoom: 16,
+            center: myLatlng
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
--- solo_event_create.php
+++ solo_event_create.php
@@ -1,5 +1,4 @@
 <?php
-
 include_once('header.php');
 
 ?>
@@ -20,8 +19,13 @@ include_once('header.php');
                 <h2>Maak hier een solo event aan voor uw fans.</h2>
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
             </div>
+            <div id="map-canvas">
+            </div>
         </div>
-
+    </div>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="js/main-solo_event_create.js"></script>
 <?php
 include_once('footer.php');
 ?>
