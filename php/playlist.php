30642fc049565f3813f94f6d23ba15ae20d78787
Sven de Ronde <ewderonde@gmail.com>
2015-04-09 15:17:06 +0100

    no message

--- php/playlist.php
+++ php/playlist.php
@@ -1,5 +1,22 @@
 <?php
-    $data = json_decode(file_get_contents("http://ws.spotify.com/search/1/album?q=foo"));
+$data = json_decode(file_get_contents("https://api.spotify.com/v1/albums/669MIsrGaRpMze2ajXgfE4/tracks"));
 
-    print_r($data);
+$i = 0;
+echo "<pre>";
+// print_r($data);
+// navigatie naar een track name:
+// $data->items[0]->name
+
+    count($data->items);
+//foreach ($data as $file) {
+//    $i++;
+//    print_r($i);
+//    print_r($file[$i]->name);
+//}
+for ($i=0; $i < count($data->items); $i++) {
+    echo "hoi";
+}
+echo "</pre>";
+
+
 ?>
