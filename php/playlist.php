3bf29b07ce53b28e8863d77122e39214ee19b00d
Dianchelo <diianchelo@gmail.com>
2015-04-09 17:34:13 +0200

    Werkende mapdata een solomap

--- php/playlist.php
+++ php/playlist.php
@@ -1,13 +1,30 @@
 <?php
 $data = json_decode(file_get_contents("https://api.spotify.com/v1/albums/669MIsrGaRpMze2ajXgfE4/tracks"));
 
-for ($i=0; $i < count($data->items); $i++) {
-    $trackData[] = array(
-        'track'=> $data->items[$i]->name,
-        'url'=> $data->items[$i]->external_urls->spotify
-    );
-}
+$i = 0;
+echo "<pre>";
+ print_r($data);
+// navigatie naar een track name:
+// $data->items[0]->name
 
-header("Content-type: application/json");
-echo json_encode($trackData);
+
+//foreach ($data as $file) {
+//    $i++;
+//    print_r($i);
+//    print_r($file[$i]->name);
+//}
+//for ($i=0; $i < count($data->items); $i++) {
+//    $trackData[] = array(
+//        'track'=> $data->items[$i]->name,
+//        'url'=> $data->items[$i]->;
+//    );
+//}
+echo "</pre>";
+
+
 ?>
+<!--$photoValues[] = array(-->
+<!--'image'=> $data->data[$i]->images->thumbnail->url,-->
+<!--'latitude'=> $data->data[$i]->location->latitude,-->
+<!--'longitude'=> $data->data[$i]->location->longitude-->
+<!--);-->
